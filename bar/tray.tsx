f7c1a83156f7b2fc13fd2f32dbee9e9eeb74d23a
Mabi19 <mateuszbartnik33@gmail.com>
2024-12-05 20:34:32 +0100

    Reformat tray module
    I originally used my shiny new nested reactivity syntax, but that's taking a long time to merge (and moving it back changed the formatting)

--- bar/tray.tsx
+++ bar/tray.tsx
@@ -4,38 +4,40 @@ import AstalTray from "gi://AstalTray";
 
 const tray = AstalTray.get_default();
 
-export const SystemTray = () => (
-    <box spacing={12}>
-        {bind(tray, "items").as((items) =>
-            items
-                .toSorted((a, b) => a.title.localeCompare(b.title))
-                .map((item) => {
-                    const menu = item.create_menu();
+export const SystemTray = () => {
+    return (
+        <box spacing={12}>
+            {bind(tray, "items").as((items) => {
+                return items
+                    .toSorted((a, b) => a.title.localeCompare(b.title))
+                    .map((item) => {
+                        const menu = item.create_menu();
 
-                    return (
-                        <button
-                            className="tray-item"
-                            tooltipText={bind(item, "tooltip_markup")}
-                            onDestroy={() => menu?.destroy()}
-                            onClick={(self, event) => {
-                                if (event.button == Astal.MouseButton.PRIMARY) {
-                                    item.activate(event.x, event.y);
-                                } else if (event.button == Astal.MouseButton.MIDDLE) {
-                                    item.secondary_activate(event.x, event.y);
-                                } else if (event.button == Astal.MouseButton.SECONDARY) {
-                                    menu?.popup_at_widget(
-                                        self,
-                                        Gdk.Gravity.SOUTH,
-                                        Gdk.Gravity.NORTH,
-                                        null
-                                    );
-                                }
-                            }}
-                        >
-                            <icon gIcon={bind(item, "gicon")} />
-                        </button>
-                    );
-                })
-        )}
-    </box>
-);
+                        return (
+                            <button
+                                className="tray-item"
+                                tooltipText={bind(item, "tooltip_markup")}
+                                onDestroy={() => menu?.destroy()}
+                                onClick={(self, event) => {
+                                    if (event.button == Astal.MouseButton.PRIMARY) {
+                                        item.activate(event.x, event.y);
+                                    } else if (event.button == Astal.MouseButton.MIDDLE) {
+                                        item.secondary_activate(event.x, event.y);
+                                    } else if (event.button == Astal.MouseButton.SECONDARY) {
+                                        menu?.popup_at_widget(
+                                            self,
+                                            Gdk.Gravity.SOUTH,
+                                            Gdk.Gravity.NORTH,
+                                            null
+                                        );
+                                    }
+                                }}
+                            >
+                                <icon gIcon={bind(item, "gicon")} />
+                            </button>
+                        );
+                    });
+            })}
+        </box>
+    );
+};
