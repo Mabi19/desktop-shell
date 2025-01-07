49445d53525a549452106f1b8c23b09ce49183fa
Mabi19 <mateuszbartnik33@gmail.com>
2025-01-07 22:30:20 +0100

    Add quick audio settings

--- bar/audio.tsx
+++ bar/audio.tsx
@@ -1,7 +1,91 @@
 import { bind, execAsync } from "astal";
+import { Gtk, hook } from "astal/gtk4";
 import AstalWp from "gi://AstalWp";
+import Pango from "gi://Pango?version=1.0";
 
 const audio = AstalWp.get_default()!.audio;
+
+const VolumeSlider = ({ device }: { device: AstalWp.Endpoint }) => {
+    const adjustment = new Gtk.Adjustment({
+        lower: 0,
+        upper: 1,
+        value: device.volume,
+        stepIncrement: 0.01,
+        pageIncrement: 0.05,
+    });
+    const scale = new Gtk.Scale({
+        adjustment,
+        hexpand: true,
+    });
+    scale.connect("change-value", (_, type, value) => {
+        value = Math.round(Math.max(0, Math.min(value * 100, 100))) / 100;
+        device.volume = value;
+        // TODO: play sounds here if this is a speaker
+    });
+    hook(scale, device, "notify::volume", () => {
+        const volume = device.volume;
+        if (Math.abs(adjustment.value - volume) > 0.001) {
+            adjustment.value = volume;
+        }
+    });
+    // TODO: disable slider when device is muted (set sensitive to false)
+
+    return scale;
+};
+
+const AudioPopover = () => {
+    function openMixer() {
+        execAsync("pavucontrol");
+        popover.popdown();
+    }
+
+    const speaker = audio.defaultSpeaker;
+    const microphone = audio.defaultMicrophone;
+
+    // TODO: mute buttons
+
+    const popover = (
+        <popover>
+            <box vertical={true}>
+                <box spacing={4}>
+                    <image iconName="audio-speakers-symbolic" />
+                    <label
+                        label={bind(speaker, "description").as((desc) => desc ?? "Speaker")}
+                        ellipsize={Pango.EllipsizeMode.END}
+                        maxWidthChars={32}
+                    />
+                </box>
+                <box>
+                    <VolumeSlider device={speaker} />
+                    <label
+                        label={bind(speaker, "volume").as((vol) => `${Math.round(vol * 100)}%`)}
+                        widthChars={5}
+                        xalign={0}
+                    />
+                </box>
+                <box spacing={4}>
+                    <image iconName="audio-input-microphone-symbolic" />
+                    <label
+                        label={bind(microphone, "description").as((desc) => desc ?? "Microphone")}
+                        ellipsize={Pango.EllipsizeMode.END}
+                        maxWidthChars={32}
+                    />
+                </box>
+                <box>
+                    <VolumeSlider device={microphone} />
+                    <label
+                        label={bind(microphone, "volume").as((vol) => `${Math.round(vol * 100)}%`)}
+                        widthChars={5}
+                        xalign={0}
+                    />
+                </box>
+
+                <button onClicked={openMixer}>Open Audio Mixer</button>
+            </box>
+        </popover>
+    ) as Gtk.Popover;
+    return popover;
+};
 
 const AudioPart = ({ device }: { device: AstalWp.Endpoint }) => {
     return (
@@ -14,7 +98,7 @@ const AudioPart = ({ device }: { device: AstalWp.Endpoint }) => {
 
 export const AudioIndicator = () => {
     return (
-        <button name="audio" onClicked={() => execAsync("pavucontrol")}>
+        <menubutton name="audio" popover={(<AudioPopover />) as Gtk.Popover}>
             <box spacing={8}>
                 {bind(audio, "defaultSpeaker").as((speaker) => (
                     <AudioPart device={speaker} />
@@ -23,6 +107,6 @@ export const AudioIndicator = () => {
                     <AudioPart device={microphone} />
                 ))}
             </box>
-        </button>
+        </menubutton>
     );
 };
