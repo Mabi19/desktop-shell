afad70240e826401aa85af207f3571cba469790f
Mabi19 <mateuszbartnik33@gmail.com>
2025-01-17 20:48:09 +0100

    Rework notifications
    There is now a concept called a "notification layout". It's essentially a subwidget that displays the title, body, image and actions.

--- notification-center/notification.tsx
+++ notification-center/notification.tsx
@@ -1,29 +1,37 @@
 import { bind } from "astal";
 import GObject, { register, signal } from "astal/gobject";
-import { App, Astal, Gdk, Gtk, astalify } from "astal/gtk4";
+import { App, Astal, Gdk, Gtk } from "astal/gtk4";
 import AstalNotifd from "gi://AstalNotifd";
+import GLib from "gi://GLib?version=2.0";
 import Pango from "gi://Pango?version=1.0";
 import { primaryMonitor } from "../utils/config";
 import { Timer } from "../utils/timer";
-import { ProgressBar } from "../widgets/progress-bar";
-
-const Inscription = astalify(Gtk.Inscription);
+import { dumpNotification } from "./notification-dump";
 
 const DEFAULT_TIMEOUT = 5000;
-const NOTIFICATION_CLEANUP_FUNCTION = Symbol();
+
+const iconTheme = Gtk.IconTheme.get_for_display(Gdk.Display.get_default()!);
+const isIcon = (name: string | null) => name && iconTheme.has_icon(name);
+
+const fileExists = (path: string | null) => path && GLib.file_test(path, GLib.FileTest.EXISTS);
+
+interface WidgetEntry {
+    widget: Gtk.Widget;
+    cleanup: () => void;
+}
 
 @register()
 class NotificationTracker extends GObject.Object {
-    #widgets: Map<number, Gtk.Widget>;
+    #widgets: Map<number, WidgetEntry>;
 
-    @signal(Gtk.Widget)
-    declare create: (widget: Gtk.Widget) => void;
+    @signal(Object)
+    declare create: (widget: WidgetEntry) => void;
 
-    @signal(Gtk.Widget, Gtk.Widget)
-    declare replace: (prev: Gtk.Widget, curr: Gtk.Widget) => void;
+    @signal(Object, Object)
+    declare replace: (prev: WidgetEntry, curr: WidgetEntry) => void;
 
-    @signal(Gtk.Widget)
-    declare destroy: (widget: Gtk.Widget) => void;
+    @signal(Object)
+    declare destroy: (widget: WidgetEntry) => void;
 
     constructor() {
         super();
@@ -33,11 +41,12 @@ class NotificationTracker extends GObject.Object {
 
         notifd.connect("notified", (_, id) => {
             console.log("notification created", id);
+            const notification = notifd.get_notification(id);
+            dumpNotification(notification);
 
             const existingWidget = this.#widgets.get(id);
-
-            const newWidget = Notification({
-                notification: notifd.get_notification(id),
+            const newWidget = NotificationWrapper({
+                notification,
             });
             this.#widgets.set(id, newWidget);
 
@@ -65,21 +74,18 @@ export const NotificationPopupWindow = () => {
 
     const box = (<box vertical={true} spacing={12} noImplicitDestroy={true}></box>) as Astal.Box;
 
-    notifs.connect("create", (_, widget: Gtk.Widget) => {
+    notifs.connect("create", (_, entry: WidgetEntry) => {
         box.add_css_class("notification-box-active");
-        box.prepend(widget);
+        box.prepend(entry.widget);
     });
-    notifs.connect("replace", (_, prev: Gtk.Widget, curr: Gtk.Widget) => {
-        box.insert_child_after(curr, prev);
-        // @ts-expect-error this is Object.assign()'ed
-        prev[NOTIFICATION_CLEANUP_FUNCTION]?.();
-        box.remove(prev);
+    notifs.connect("replace", (_, prev: WidgetEntry, curr: WidgetEntry) => {
+        box.insert_child_after(curr.widget, prev.widget);
+        box.remove(prev.widget);
+        prev.cleanup();
     });
-    notifs.connect("destroy", (_, widget: Gtk.Widget) => {
-        console.log("removing", widget);
-        box.remove(widget);
-        // @ts-expect-error this is Object.assign()'ed
-        widget[NOTIFICATION_CLEANUP_FUNCTION]?.();
+    notifs.connect("destroy", (_, entry: WidgetEntry) => {
+        box.remove(entry.widget);
+        entry.cleanup();
         if (box.get_first_child() == null) {
             box.remove_css_class("notification-box-active");
         }
@@ -104,26 +110,21 @@ export const NotificationPopupWindow = () => {
     );
 };
 
-const NotificationIcon = ({ notification }: { notification: AstalNotifd.Notification }) => {
-    if (notification.image) {
-        return <image iconName={notification.image} cssClasses={["icon"]} />;
-    }
-    if (notification.appIcon) {
-        return <image iconName={notification.appIcon} cssClasses={["icon"]} />;
-    } else if (notification.desktopEntry) {
-        return <image iconName={notification.desktopEntry} cssClasses={["icon"]} />;
-    } else {
-        return <image iconName="dialog-information-symbolic" cssClasses={["icon"]} />;
-    }
-};
-
-const Notification = ({ notification }: { notification: AstalNotifd.Notification }) => {
+// This widget provides the constant parts of the notification.
+// It also chooses a suitable layout for the notification.
+function NotificationWrapper({
+    notification,
+}: {
+    notification: AstalNotifd.Notification;
+}): WidgetEntry {
     console.log("got notification! timeout:", notification.expireTimeout);
+    const NOTIFICATION_WIDTH = 400;
+
     // TODO: Replace this with the frame clock?
     const timer = new Timer(
         notification.expireTimeout == -1 ? DEFAULT_TIMEOUT : notification.expireTimeout
     );
-    const progressBar = new Gtk.ProgressBar({ fraction: 0 });
+    const progressBar = new Gtk.ProgressBar({ fraction: 1 });
     const cleanup = timer.subscribe(() => {
         progressBar.fraction = 1 - timer.timeLeft / timer.timeout;
 
@@ -144,70 +145,113 @@ const Notification = ({ notification }: { notification: AstalNotifd.Notification
         }
     }
 
-    // TODO: rework layout
-    // Have multiple layouts switched between with heuristics
+    let icon: Gtk.Widget | null;
+    if (notification.appIcon) {
+        if (fileExists(notification.appIcon)) {
+            icon = Gtk.Image.new_from_file(notification.appIcon);
+        } else {
+            icon = Gtk.Image.new_from_icon_name(notification.appIcon);
+        }
+    } else if (isIcon(notification.desktopEntry)) {
+        icon = Gtk.Image.new_from_icon_name(notification.desktopEntry);
+    } else {
+        icon = null;
+    }
+
+    return {
+        cleanup,
+        widget: (
+            <box
+                onHoverEnter={() => timer.pauseCount++}
+                onHoverLeave={() => timer.pauseCount--}
+                onButtonPressed={(_box, event) => handleDefaultClick(event)}
+                vertical={true}
+                hexpand={false}
+                widthRequest={NOTIFICATION_WIDTH}
+                cssClasses={["notification"]}
+                overflow={Gtk.Overflow.HIDDEN}
+            >
+                <box cssClasses={["header"]} spacing={8}>
+                    {icon}
+                    <label label={notification.appName} halign={Gtk.Align.START} hexpand={true} />
+                    <button
+                        halign={Gtk.Align.END}
+                        iconName="window-close-symbolic"
+                        cssClasses={["close-button"]}
+                        onClicked={() => {
+                            notification.dismiss();
+                        }}
+                    />
+                </box>
+                <Gtk.Separator
+                    orientation={Gtk.Orientation.HORIZONTAL}
+                    cssClasses={["header-separator"]}
+                />
+                <NotificationLayoutProfile notification={notification} />
+                {progressBar}
+            </box>
+        ),
+    };
+}
+
+const NotificationLayoutProfile = ({
+    notification,
+}: {
+    notification: AstalNotifd.Notification;
+}) => {
+    // TODO: rework
 
     // TODO: animations
     // TODO: urgency (low: dimmed progress bar, normal: regular progress bar, critical: red border?)
     // TODO: move into notification center
 
-    const NOTIFICATION_WIDTH = 400;
-    const NOTIFICATION_PADDING = 8;
-
-    // progress bar is on the outside so that it can hug the edge
     return (
-        <box
-            onHoverEnter={() => timer.pauseCount++}
-            onHoverLeave={() => timer.pauseCount--}
-            onButtonPressed={(_eventBox, event) => handleDefaultClick(event)}
-            vertical={true}
-            hexpand={false}
-            vexpand={false}
-            widthRequest={NOTIFICATION_WIDTH}
-            cssClasses={["notification"]}
-            overflow={Gtk.Overflow.HIDDEN}
-            // TODO: Do something a little less janky here. In fact, actually just return a meta-object that has some methods.
-            setup={(self) => Object.assign(self, { [NOTIFICATION_CLEANUP_FUNCTION]: cleanup })}
-        >
-            <box hexpand={false} vertical={true} cssClasses={["content"]} spacing={8}>
-                <box spacing={8}>
-                    <NotificationIcon notification={notification} />
-                    <label label={notification.summary} cssClasses={["title"]} xalign={0} />
-                    <button onButtonPressed={() => notification.dismiss()}>
-                        <image iconName="window-close-symbolic" />
-                    </button>
-                </box>
+        <box hexpand={false} vertical={true} cssClasses={["layout", "layout-profile"]} spacing={4}>
+            <box spacing={8}>
+                {/* TODO: test if that's a file or a named icon (see example) */}
+                <image
+                    iconName="dialog-information-symbolic"
+                    visible={Boolean(notification.image)}
+                />
                 <label
-                    // Use U+2028 LINE SEPARATOR in order to not introduce paragraph breaks.
-                    label={notification.body.replaceAll("\n", "\u2028")}
-                    cssClasses={["description"]}
-                    useMarkup={true}
+                    label={notification.summary}
+                    cssClasses={["title"]}
                     wrap={true}
-                    ellipsize={Pango.EllipsizeMode.MIDDLE}
-                    // Setting this to a value that is definitely smaller than the box width
-                    // causes the label to expand to that size.
+                    ellipsize={Pango.EllipsizeMode.END}
+                    lines={2}
                     maxWidthChars={5}
-                    lines={3}
                     halign={Gtk.Align.FILL}
                     xalign={0}
                 />
-
-                {notification.get_actions().length > 0 ? (
-                    <box spacing={8}>
-                        {notification.get_actions().map((action) => (
-                            <button
-                                onButtonPressed={() => notification.invoke(action.id)}
-                                hexpand={true}
-                            >
-                                {action.label}
-                            </button>
-                        ))}
-                    </box>
-                ) : (
-                    false
-                )}
             </box>
-            {progressBar}
+            <label
+                // Use U+2028 LINE SEPARATOR in order to not introduce paragraph breaks.
+                label={notification.body.replaceAll("\n", "\u2028")}
+                cssClasses={["description"]}
+                useMarkup={true}
+                wrap={true}
+                ellipsize={Pango.EllipsizeMode.MIDDLE}
+                // Setting this to a value that is definitely smaller than the box width
+                // causes the label to expand to that size.
+                maxWidthChars={5}
+                lines={5}
+                halign={Gtk.Align.FILL}
+                xalign={0}
+            />
+
+            {/* TODO: Move this into a separate component */}
+            {notification.get_actions().length > 0 ? (
+                <box spacing={8}>
+                    {notification.get_actions().map((action) => (
+                        <button
+                            onButtonPressed={() => notification.invoke(action.id)}
+                            hexpand={true}
+                        >
+                            {action.label}
+                        </button>
+                    ))}
+                </box>
+            ) : null}
         </box>
     );
 };
